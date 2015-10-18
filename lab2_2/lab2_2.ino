462cef08bab21e7aa35bd595b0e8e339d49c5316
RishYang <yangrshin@gmail.com>
2015-10-19 02:45:30 +0800

    Arduino setup

--- lab2_2/lab2_2.ino
+++ lab2_2/lab2_2.ino
@@ -1,6 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
-
+    int ledPin = 13;
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, LOW);
+    int button = 7;
+    pinMode(button, INPUT); 
+    
 }
 
 void loop() {
