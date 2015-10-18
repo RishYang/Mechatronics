e92d5c35552ce3de6fc8710b4ff074911c243b99
RishYang <yangrshin@gmail.com>
2015-10-19 03:33:35 +0800

    first complete version

--- lab2_2/lab2_2.ino
+++ lab2_2/lab2_2.ino
@@ -1,14 +1,40 @@
+int state = 0;
+int old_t=0;
+int delay_t=100;
+int ledPin = 13;
+int button = 7;
 void setup() {
   // put your setup code here, to run once:
-    int ledPin = 13;
+    
     pinMode(ledPin, OUTPUT);
     digitalWrite(ledPin, LOW);
-    int button = 7;
     pinMode(button, INPUT); 
+
+    Serial.begin(9600);
+    Serial.print("Hello, this message is from Arduino Uno");
     
+    state = LOW;
+    old_t=millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    int new_t=millis();
+    if(new_t-old_t>=delay_t){
+        switch (state){
+            case (LOW):
+                state = HIGH;
+                digitalWrite(ledPin, state);
+                old_t=new_t;
+            break;
+            case (HIGH):
+                state = LOW;
+                digitalWrite(ledPin, state);
+                old_t=new_t;
+            break;
+        }
+    }
+    if(digitalRead(button)){
+        delay_t = delay_t + 50;
+        delay_t = delay_t % 1000;
+    }
 }
